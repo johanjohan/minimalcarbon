495f943b11f4b4188d6fac8dcb3aa4062462e06d
johanjohan <github.com@particles.de>
2022-06-16 22:22:10 +0200

    win

--- exe_sitemap_crawl.py
+++ exe_sitemap_crawl.py
@@ -12,7 +12,9 @@ import colorama
 # pip install python-magic-bin # win
 import magic
 import time
-
+import urllib.request
+import ssl
+        
 # init the colorama module
 colorama.init()
 GREEN = colorama.Fore.GREEN
@@ -42,7 +44,9 @@ mime_types_allowed = ["text/html", "text/plain"]
 def_url = "https://1001suns.com/sitemap_post/" # for args
 def_url = "https://1001suns.com/universe_bochum21/" # for args
 def_url = "https://karlsruhe.digital/"
-def_max_urls = 1000
+def_max_urls = 1
+
+args    = None
 
 #-----------------------------------------
 # 
@@ -79,8 +83,6 @@ def get_encoding(url):
     
 def get_mime_type(url):
     try:
-        import urllib.request
-        import ssl
         req = urllib.request.Request(url, data=None, headers=headers) # user agent
         # https://stackoverflow.com/questions/27835619/urllib-and-ssl-certificate-verify-failed-error
         context = ssl._create_unverified_context()
@@ -92,8 +94,21 @@ def get_mime_type(url):
     except Exception as e:
         print(f"{RED}[!] {url} Exception: {e}{RESET}")
         return None
-    
-def get_all_website_links(url):
+
+def get_status_code(url, fast=True, timeout=5):
+    try:
+        if fast:
+            r = requests.head(url,verify=True,timeout=timeout) # it is faster to only request the header
+            return (r.status_code)
+        else:
+            req = urllib.request.Request(url, timeout=timeout, data=None, headers=headers) # user agent
+            context = ssl._create_unverified_context()
+            with urllib.request.urlopen(req, context=context) as response:
+                return response.getcode()
+    except:
+        return -1
+
+def get_all_website_links(url, max_urls):
     """
     Returns all URLs that is found on `url` in which it belongs to the same website
     """
@@ -137,7 +152,7 @@ def get_all_website_links(url):
         
         if not href in internal_urls:
             print(
-                f"{total_urls_visited}/{def_max_urls}", 
+                f"{total_urls_visited}/{max_urls}", 
                 mime_type, 
                 f"{GREEN}[*] Internal: {href}{RESET}"
             )
@@ -147,7 +162,7 @@ def get_all_website_links(url):
         
     return urls
 
-def crawl(url, max_urls=30):
+def crawl(url, max_urls):
     """
     Crawls a web page and extracts all links.
     You'll find all links in `external_urls` and `internal_urls` global set variables.
@@ -157,58 +172,145 @@ def crawl(url, max_urls=30):
     global total_urls_visited
     total_urls_visited += 1
     print(f"{YELLOW}[*] Crawling: {url}{RESET}")
-    links = get_all_website_links(url)
+    links = get_all_website_links(url, max_urls)
     for link in links:
         if total_urls_visited > max_urls:
             break
         crawl(link, max_urls=max_urls)
 
+
 #-----------------------------------------
 # main
 #-----------------------------------------
 if __name__ == "__main__":
-    start_secs = time.time()
-    
+
     import argparse
     parser = argparse.ArgumentParser(description="Link Extractor Tool with Python")
     parser.add_argument("--url",        default=def_url,        type=str,   help="The URL to extract links from.")
     parser.add_argument("--max-urls",   default=def_max_urls,   type=int,   help="Number of max URLs to crawl.")
+    parser.add_argument("--crawl",      default=True,           type=bool,  help="crawl True or False")
     
     #args = parser.parse_args()
     args, _unknown_args = parser.parse_known_args()
-    url = args.url
-    max_urls = args.max_urls
-    print("url     :", url)
-    print("max_urls:", max_urls)
-
-    crawl(url, max_urls=max_urls)
-
-    print("[+] Total Internal links:", len(internal_urls))
-    print("[+] Total External links:", len(external_urls))
-    print("[+] Total URLs:", len(external_urls) + len(internal_urls))
-    print("[+] Total crawled URLs:", max_urls)
-
-    domain_name = urlparse(url).netloc
-    print("domain_name:", domain_name)
-
-    # skip http
-    print("before http replace:", len(internal_urls))
-    internal_urls = [s.replace('http://', 'https://') for s in internal_urls]
-    print("after http replace:", len(internal_urls))
-    
-    internal_urls = sorted(set(internal_urls))
-    external_urls = sorted(set(external_urls))
-    
-    # save the internal links to a file
-    print("save the internal links to a file...")
-    with open(f"{data_folder}/{domain_name}_internal_links.csv", "w", encoding="utf-8") as f:
-        for internal_link in internal_urls:
-            f.write(internal_link.strip() + "\n")
-
-    # save the external links to a file
-    print("save the external links to a file...")
-    with open(f"{data_folder}/{domain_name}_external_links.csv", "w", encoding="utf-8") as f:
-        for external_link in external_urls:
-            f.write(external_link.strip() + "\n")
+    print("url     :", args.url)
+    print("max_urls:", args.max_urls)
             
-    print("all done:", "duration:", round(time.time() - start_secs, 1), "s")
+    
+    #-----------------------------------------
+    # crawl
+    #-----------------------------------------
+    start_secs = time.time()
+
+    domain_name = urlparse(args.url).netloc
+    print("domain_name:", domain_name)
+    file_internal_path = f"{data_folder}/{domain_name}_internal_links.csv"
+    file_external_path = f"{data_folder}/{domain_name}_external_links.csv"
+
+    if args.crawl:
+        crawl(args.url, max_urls=args.max_urls)
+
+        print("[+] Total Internal links:", len(internal_urls))
+        print("[+] Total External links:", len(external_urls))
+        print("[+] Total URLs:", len(external_urls) + len(internal_urls))
+        print("[+] Total crawled URLs:", args.max_urls)
+
+        # skip http
+        print("before http replace:", len(internal_urls))
+        internal_urls = [s.replace('http://', 'https://') for s in internal_urls]
+        print("after http replace:", len(internal_urls))
+        
+        internal_urls = sorted(set(internal_urls))
+        external_urls = sorted(set(external_urls))
+        
+        # save the internal links to a file
+        print("save the internal links to a file...")
+        with open(file_internal_path, "w", encoding="utf-8") as f:
+            for internal_link in internal_urls:
+                #check response
+                status = get_status_code(internal_link)
+                print(status, internal_link)
+                if status in [200, 301]:
+                    f.write(internal_link.strip() + "\n")
+                else:
+                    print(f"{RED}[*] status: {status}{RESET}")
+
+        # save the external links to a file
+        print("save the external links to a file...")
+        with open(file_external_path, "w", encoding="utf-8") as f:
+            for external_link in external_urls:
+                f.write(external_link.strip() + "\n")
+                
+        print("all done:", "duration:", round(time.time() - start_secs, 1), "s")
+    
+    exit(0)
+    #-----------------------------------------
+    # pywebcopy
+    #-----------------------------------------
+    from pywebcopy import save_webpage
+    
+    with open(file_internal_path, "r") as f:
+        internal_urls = f.readlines()
+        
+    for internal_link in internal_urls:
+        print(f"{YELLOW}[*] save_webpage: {internal_link}{RESET}")
+        # save_webpage(
+        #     url=internal_link,
+            
+        #     project_folder="scraped_pywebcopy/",
+        #     project_name=urlparse(args.url).netloc,
+            
+        #     debug=True,
+        #     open_in_browser=True,
+        #     delay=None,
+        #     bypass_robots= True,
+            
+        #     threaded=False,
+        # )  
+    
+    exit(0)  
+    
+    # https://stackoverflow.com/questions/31205497/how-to-download-a-full-webpage-with-a-python-script
+    
+    import urllib.request as urllib2
+    from bs4 import *
+    from urllib.parse  import urljoin
+
+
+    def crawl(pages, depth=None):
+        indexed_url = [] # a list for the main and sub-HTML websites in the main website
+        for i in range(depth):
+            for page in pages:
+                if page not in indexed_url:
+                    indexed_url.append(page)
+                    try:
+                        c = urllib2.urlopen(page)
+                    except:
+                        print( "Could not open %s" % page)
+                        continue
+                    soup = BeautifulSoup(c.read())
+                    links = soup('a') #finding all the sub_links
+                    for link in links:
+                        if 'href' in dict(link.attrs):
+                            url = urljoin(page, link['href'])
+                            if url.find("'") != -1:
+                                    continue
+                            url = url.split('#')[0] 
+                            if url[0:4] == 'http':
+                                    indexed_url.append(url)
+            pages = indexed_url
+        return indexed_url
+
+
+    pagelist=["https://en.wikipedia.org/wiki/Python_%28programming_language%29"]
+    urls = crawl(pagelist, depth=1)
+    print( urls )
+    
+    exit(0)    
+    
+    # wget -E -H -k -K -p -e robots=off https://karlsruhe.digital
+    # wget -E --span-hosts -k -K -p -e robots=off https://karlsruhe.digital
+    # wget --mirror -nH -np -p -k -E -e robots=off https://karlsruhe.digital
+    # wget --mirror -nH -np -p -k -E -e robots=off -i "../data/karlsruhe.digital_internal_links.csv" 
+    
+    # https://stackoverflow.com/questions/31205497/how-to-download-a-full-webpage-with-a-python-script
+    
