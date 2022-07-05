4169ba7586fb639753890dd52ebbd79ea7bfee4a
johanjohan <github.com@particles.de>
2022-07-05 22:43:48 +0200

    without effort, sublime...

--- app_200_images.py
+++ app_200_images.py
@@ -964,6 +964,7 @@ if __name__ == "__main__":
             This is the environmentally friendly twin of 
             .<br/>The energy consumption of this website was reduced by {saved_string}.
             .<br/>Der Energieverbrauch dieser Website wurde um {saved_string} reduziert.
+            This is the Low Carbon proxy of {same_page_link}
             """
             # https://babel.pocoo.org/en/latest/dates.html
             from babel.dates import format_date, format_datetime, format_time
@@ -972,11 +973,11 @@ if __name__ == "__main__":
             saved_string = f"<span style=''>{perc100_saved:.1f}%</span>"
             if "/en/" in wp_path:
                 dt_string = format_date(dt, format=format, locale='en')
-                banner_header_text = f"{config.svg_leaf_img} This is the Low Carbon proxy of {same_page_link}" # <br/>{svg_percircle}
+                banner_header_text = f"{config.svg_leaf_img} This is the Low Carbon Gateway of {same_page_link}" # <br/>{svg_percircle}
                 banner_footer_text = f"{svg_percircle}<br/>unpowered by {config.html_infossil_link}" # <br/>{dt_string}
             else:
                 dt_string = format_date(dt, format=format, locale='de_DE')
-                banner_header_text = f"{config.svg_leaf_img} Dies ist der Low Carbon Proxy von {same_page_link}"
+                banner_header_text = f"{config.svg_leaf_img} Dies ist der Low Carbon Gateway von {same_page_link}"
                 banner_footer_text = f"{svg_percircle}<br/>unpowered by {config.html_infossil_link}" # <br/>{dt_string}
                 
             #---------------------------
